3b406328cae29b0e167713ab1e31dced5d618163
Zakru <sakari.leukkunen@gmail.com>
2019-11-14 00:13:43 +0200

    Import urllib submodules separately

--- wynn/requests.py
+++ wynn/requests.py
@@ -20,9 +20,9 @@ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-
 import json
-import urllib
+import urllib.request
+import urllib.parse
 
 def requestList(url):
 	"""Requests a list of objects from the Wynncraft API.
