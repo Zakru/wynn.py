f2aebfa0f1fc22bbf6d851fdaf5f976817eecd38
Zakru <sakari.leukkunen@gmail.com>
2019-11-14 00:23:25 +0200

    Finish get_level.py example

---
+++ examples/get_level.py
@@ -0,0 +1,25 @@
+"""
+Gets the level of a user's class.
+"""
+
+import sys
+
+from wynn.player import getPlayer
+
+def getLevel():
+	user = sys.argv[1]
+	cls = sys.argv[2]
+	
+	print(getPlayer(user).classesDict[cls].level)
+
+usage = """
+Usage:
+  python get_level.py <user> <class>
+
+Gets the level of the specified class of the specified Wynncraft user.
+"""
+
+if sys.argv[1] == '--help':
+	print(usage)
+else:
+	getLevel()
