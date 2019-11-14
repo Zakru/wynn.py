d8ec50f725c49bfd4fe73415d0451c2c648f7ba5
Zakru <sakari.leukkunen@gmail.com>
2019-11-14 14:26:21 +0200

    Add more information to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+	readme = f.read()
+
 setup(
 	name='wynn.py',
 	version='0.1.0a',
 	author='Zakru',
 	url='https://github.com/Zakru/wynn.py',
 	description='A Python wrapper for the Wynncraft public API',
+	long_description=readme,
+	long_description_content_type='text/markdown',
+	project_urls={
+		'Documentation': 'https://wynnpy.readthedocs.io/en/latest/',
+		'Source Code': 'https://github.com/Zakru/wynn.py',
+		'Issue Tracker': 'https://github.com/Zakru/wynn.py/issues',
+	},
 	packages=['wynn'],
 	classifiers=[
 		'Development Status :: 3 - Alpha',
