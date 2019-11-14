78041fd716e58f8d24470cbeaf38fac5abfe9404
Zakru <sakari.leukkunen@gmail.com>
2019-11-14 10:42:06 +0200

    Set version properly

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Zakru'
 author = 'Zakru'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0a'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='wynn.py',
-	version='0.1.0',
+	version='0.1.0a',
 	author='Zakru',
 	url='https://github.com/Zakru/wynn.py',
 	description='A Python wrapper for the Wynncraft public API',
